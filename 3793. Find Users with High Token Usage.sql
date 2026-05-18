048208ddf2a304412fd2f983167660083a7c2bb2
Suraj Doss <suraj_sebastien@yahoo.com>
2026-05-18 22:53:53 +0530

    Add SQL query to find users with high token usage

---
+++ 3793. Find Users with High Token Usage.sql
@@ -0,0 +1,5 @@
+SELECT user_id, COUNT(prompt) AS prompt_count, ROUND( AVG( tokens ), 2) AS avg_tokens
+FROM prompts
+GROUP BY user_id
+HAVING COUNT(prompt) >= 3 AND MAX(tokens) > ROUND( AVG( tokens ), 2) 
+ORDER BY avg_tokens DESC
