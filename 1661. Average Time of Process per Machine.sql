cce29d94d0ea46d4824cda4dd35c18c204407624
Suraj Doss <suraj_sebastien@yahoo.com>
2026-05-19 23:11:56 +0530

    Add SQL query for average processing time per machine

---
+++ 1661. Average Time of Process per Machine.sql
@@ -0,0 +1,5 @@
+SELECT start.machine_id, ROUND( AVG( end.timestamp - start.timestamp ), 3) as processing_time
+FROM activity start
+JOIN activity end ON start.machine_id = end.machine_id AND start.process_id = end.process_id
+WHERE start.activity_type = 'start' AND end.activity_type = 'end'
+GROUP BY machine_id
